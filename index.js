1b1601f654f7b962a94afcffcd862289eb441f1f
daViEbaISmotanite <124218450+daViEbaISmotanite@users.noreply.github.com>
2023-02-06 13:47:29 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
-let notified = [];
+const express = require('express');
+const webpush = require('web-push');
+const mongoose = require('mongoose');
+const path = require('path');
 
-function notifier() {
-    DB.forEach(async (mace) => {
-        try {
-            let temp = await axios(`https://stripchat.com/api/external/v4/widget?sortOrder=asc&modelsList=${mace}&strict=1`);
-            //console.log(temp.data.models[0]);
-            if (temp.data.models[0] && !notified.includes(mace)) {
-                notified.push(mace);
-                let prms = Notification.requestPermission();
-                let notification = new Notification(`${temp.data.models[0].username} is online`);
-                notification.onclick = function(e) {
-                    e.preventDefault();
-                    window.open(`https://xhamsterlive.com/${mace}`, '_blank');
-                };
-            }
+//---VAPID keys should be generated only once.---
+const vapidKeys = webpush.generateVAPIDKeys();
+//---Prints 2 URL Safe Base64 Encoded Strings---
+//console.log(vapidKeys.publicKey, vapidKeys.privateKey);
 
-            if(!temp.data.models[0] && notified.includes(mace)) {
-                notified.splice(notified.indexOf(mace), 1);
-                console.log(`Iztri ${mace}`);
-            }
-        } catch (error) {
-            throw Error(error);
-        }
-    });
+const db_URL = 'mongodb+srv://hamstercho:RG2haRaQJZkaEScK@maincluster.bia7isc.mongodb.net/hamstercho?retryWrites=true&w=majority';
+mongoose.set("strictQuery", false);
+mongoose.connect(db_URL, console.log('DB is connected'));
 
-    setTimeout(notifier, "60000");
-}
+const app = express();
+const Macurana = mongoose.model('Macurana', new mongoose.Schema({ ime: { type: String, required: true } }));
 
-notifier();
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'client')));
+//const publicKey = 'BMj-xaKrqOER3-CUjC8yhuHXV1mPbHeLQsoZl9v9je6yr5C2ht0yz92ZGODpNcK-RMOAUYa5siz6c5T8YMJwl2M';
+//const privateKey = 'MbWkJxR9szxiWP-afoTW6wGVjBn73s87CFqZmpVrugY';
+webpush.setVapidDetails('mailto:username:yabadabani@mail.bg', vapidKeys.publicKey, vapidKeys.privateKey);
+
+app.post('/macki', async (req, res) => {
+    try {
+        const macki = await Macurana.find({}).lean();
+        res.status(201).json({});
+        macki.forEach(ime => {
+            const haber = JSON.stringify({ title: 'Na batyu kakite prashtat haber' });
+            webpush.sendNotification(ime, haber).catch(err => console.error(err));
+        });
+    } catch (mackiErr) {
+        console.error(mackiErr);
+    }
+});
+
+app.listen(3310, console.log('Server is listening on port: 3310'));
